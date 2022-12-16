4e6315eb01aac8a8f0735e837b870664c81194cb
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-16 19:44:38 +0300

    task06 finished

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.Write("number is even");
+}
+else
+{
+    Console.Write("number is odd");
+}
